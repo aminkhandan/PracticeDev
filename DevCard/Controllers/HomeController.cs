3f3ffc37ff197c5aee3d5400d006a3f566a32c74
khandan <amin.khandan@yahoo.com>
2022-04-01 14:37:58 +0430

    attribute base routing

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -4,51 +4,53 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCard.Controllers;
-
+[Route("/t/t")]
 public class HomeController : Controller
-{
-    private readonly List<Service> _services = new List<Service>
     {
-        new Service(1,"طلایی"),
-        new Service(2,"نقره ای"),
-        new Service(3,"خاکستری"),
-        new Service(4,"برنز")
-    };
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Contact()
-    {
-        var form = new Contact
+        private readonly List<Service> _services = new List<Service>
         {
-            Services = new SelectList(_services,"Id","Name")
+            new Service(1, "طلایی"),
+            new Service(2, "نقره ای"),
+            new Service(3, "خاکستری"),
+            new Service(4, "برنز")
         };
-        return View(form);
-    }
-
-    [HttpPost]
-    public IActionResult Contact(Contact form)
-    {
-        form.Services = new SelectList(_services, "Id", "Name");
-        if (!ModelState.IsValid)
+        [Route("myindex")]
+        public IActionResult Index()
         {
-            ViewBag.error = "لطفا مجدد تلاش نمایید";
+            return View();
+        }
+        [HttpGet("contactpage")]
+        public IActionResult Contact()
+        {
+            var form = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
             return View(form);
         }
-        ModelState.Clear();
-        form = new Contact
-        {
-            Services = new SelectList(_services, "Id", "Name")
-        };
-        ViewBag.success = "پیام شما با موفقیت ارسال شد";
-        return View(form);
-    }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        [HttpPost]
+        public IActionResult Contact(Contact form)
+        {
+            form.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "لطفا مجدد تلاش نمایید";
+                return View(form);
+            }
+
+            ModelState.Clear();
+            form = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+            ViewBag.success = "پیام شما با موفقیت ارسال شد";
+            return View(form);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
     }
-}
